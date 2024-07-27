01b36fc10825d95fe5bf86ea6fa3b31a2c8642ed
Karen Judelyn Fernandes <116041695+karlyndiary@users.noreply.github.com>
2024-07-27 12:44:43 +0530

    Update Database & Table Creation.sql

--- [01] Web Scraping & ETL/Database & Table Creation.sql
+++ [01] Web Scraping & ETL/Database & Table Creation.sql
@@ -4,11 +4,11 @@ USE Flipkart;
 
 CREATE TABLE Laptops (
     id INT PRIMARY KEY,
-    title NVARCHAR(255),
+    title NVARCHAR(400),
     price NVARCHAR(10),
     original_price NVARCHAR(10),
     discount NVARCHAR(10),
-    star NVARCHAR(10),
+    star FLOAT(10),
     review NVARCHAR(50),
-    description NVARCHAR(255)
+    description NVARCHAR(600)
 );
