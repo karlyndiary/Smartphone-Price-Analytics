25f0139ee367e0a589593c003603e8e1cc91ac4c
Karen Judelyn Fernandes <116041695+karlyndiary@users.noreply.github.com>
2024-07-31 12:46:04 +0530

    Update [02] Query the required columns.sql

--- [02] Data Cleaning in SQL Server/[02] Query the required columns.sql
+++ [02] Data Cleaning in SQL Server/[02] Query the required columns.sql
@@ -1,1 +1,12 @@
 # Querying the required columns for analysis
+
+INSERT INTO [Flipkart].[dbo].[PhoneDetails] (
+    title, price, original_price, discount, star, brand, model, camera, display, 
+    screen_size_inch, ram, storage, color, processor, battery, review, rating
+)
+SELECT title, MIN(price) AS price, original_price, MIN(discount) AS discount, star, brand, model, camera, display, 
+       screen_size_inch, ram, storage, color, processor, battery, review, rating
+FROM [Flipkart].[dbo].[Phones]
+WHERE processor IS NOT NULL
+GROUP BY title, original_price, star, brand, model, camera, display, screen_size_inch, ram, storage, color, 
+processor, battery, review, rating;
