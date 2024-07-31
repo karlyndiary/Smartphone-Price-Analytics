2cf89d224fec6f3128a9ccba92d9a5b196c9a562
Karen Judelyn Fernandes <116041695+karlyndiary@users.noreply.github.com>
2024-07-31 11:02:21 +0530

    Update [04] Database & Table Creation.sql

--- [01] Web Scraping & ETL/[04] Database & Table Creation.sql
+++ [01] Web Scraping & ETL/[04] Database & Table Creation.sql
@@ -2,13 +2,27 @@ CREATE DATABASE Flipkart;
 
 USE Flipkart;
 
-CREATE TABLE Laptops (
+CREATE TABLE Phones (
     id INT PRIMARY KEY,
-    title NVARCHAR(400),
-    price NVARCHAR(10),
-    original_price NVARCHAR(10),
-    discount NVARCHAR(10),
-    star FLOAT(10),
-    review NVARCHAR(50),
-    description NVARCHAR(600)
+    title VARCHAR(255),
+    price INT,
+    original_price INT,
+    discount INT,
+    star FLOAT,
+    ratings_reviews VARCHAR(500),
+    description TEXT,
+    brand VARCHAR(100),
+    model VARCHAR(100),
+    camera VARCHAR(100),
+    screen_size_cm FLOAT,
+    screen_size_inch FLOAT,
+    display VARCHAR(100),
+    ram VARCHAR(50),
+    storage VARCHAR(50),
+    expandable_storage VARCHAR(50),
+    color VARCHAR(50),
+    processor VARCHAR(100),
+    battery VARCHAR(100),
+    rating INT,
+    review INT
 );
