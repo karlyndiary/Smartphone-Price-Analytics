0b80f51eed3e03bff4d527043edb7ddf67e3b2a5
Karen Judelyn Fernandes <116041695+karlyndiary@users.noreply.github.com>
2024-08-28 17:54:19 +0530

    Update [01] View.sql

--- [02] SQL Table Setup and Analytical Queries/[01] View.sql
+++ [02] SQL Table Setup and Analytical Queries/[01] View.sql
@@ -4,3 +4,8 @@ CREATE VIEW [dbo].[PhonesView] AS
 SELECT [pid], [title], [brand], [stock], [mrp], [price], [first_image_url] as image, [model], [screen_size_inch] as screen_size, [display], 
 	   [ram], [storage], [color], [processor], [battery], [average_rating] as rating, [rear_camera], [front_camera]
   FROM [Flipkart].[dbo].[Phones]
+
+CREATE VIEW [dbo].[PhonesViews] AS
+SELECT [pid], [title], [brand], [stock], [mrp], [price], [price_range], [model], [screen_size_inch] as screen_size, [display], 
+	   [ram], [storage], [color], [processor], [battery], [average_rating] as rating, [rear_camera], [front_camera]
+  FROM [Flipkart].[dbo].[Phones]
