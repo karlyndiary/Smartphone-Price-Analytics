aeeeecf47a3d6494406ec668c7cdf5716bb830f4
Karen Judelyn Fernandes <116041695+karlyndiary@users.noreply.github.com>
2024-08-28 18:10:12 +0530

    Create [04] Battery Capacity above average.sql

---
+++ [02] SQL Table Setup and Analytical Queries/[04] Battery Capacity above average.sql
@@ -0,0 +1,6 @@
+-- Which phones have a battery capacity above the average?
+
+SELECT title, battery
+FROM [Flipkart].[dbo].[PhonesView]
+WHERE battery > (SELECT avg(battery)
+				 FROM [Flipkart].[dbo].[PhonesView])
