011b7c5cc9798622c30f00898cc8c020afcf1f2e
Karen Judelyn Fernandes <116041695+karlyndiary@users.noreply.github.com>
2024-08-28 18:10:53 +0530

    Update [02] Average price by brand.sql

--- [02] SQL Table Setup and Analytical Queries/[02] Average price by brand.sql
+++ [02] SQL Table Setup and Analytical Queries/[02] Average price by brand.sql
@@ -1,5 +1,5 @@
 -- What is the average price of phones by brand?
 SELECT brand, avg(price) as avg_price 
-FROM Flipkart.dbo.PhonesView
+FROM [Flipkart].[dbo].[PhonesView]
 GROUP BY brand
 ORDER BY avg_price DESC;
