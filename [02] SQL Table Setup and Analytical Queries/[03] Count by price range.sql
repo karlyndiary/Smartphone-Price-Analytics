3cf45aa5a26cb20a30e6216b1c67ec83606b9783
Karen Judelyn Fernandes <116041695+karlyndiary@users.noreply.github.com>
2024-08-28 16:02:10 +0530

    Update [03] Count by price range.sql

--- [02] SQL Table Setup and Analytical Queries/[03] Count by price range.sql
+++ [02] SQL Table Setup and Analytical Queries/[03] Count by price range.sql
@@ -1,1 +1,14 @@
+-- Adding a new column price_range
+ALTER TABLE Flipkart.dbo.Phones
+ADD price_range VARCHAR(10);
+
+-- Update price_range column
+UPDATE Flipkart.dbo.Phones
+SET price_range = 
+	CASE 
+		WHEN price < 10000 THEN 'LOW'
+		WHEN price < 20000 THEN 'MID'
+		ELSE 'HIGH'
+	END
+
 -- How many phones fall into each price range (Low, Mid, High)?
